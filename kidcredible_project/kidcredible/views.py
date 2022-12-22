206619814db5bcf8978484d59849982c1de37909
Justin Harris <justin.r.harris@gmail.com>
2022-12-22 14:42:35 -0500

    added views.py info

--- kidcredible_project/kidcredible/views.py
+++ kidcredible_project/kidcredible/views.py
@@ -1,4 +1,35 @@
-# kidcredible/views.py
-from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import UserSerializer, ProductSerializer, ReviewSerializer
 from .models import User, Product, Review
+
+# Create your views here.
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
---
+++ kidcredible_project/settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE kidcredible;
+CREATE USER kidcredibleuser WITH PASSWORD 'kidcredible';
+GRANT ALL PRIVILEGES ON DATABASE kidcredible TO kidcredibleuser;
