4979932090a4094bc6933bc14cdf4dbf6793b05f
Justin Harris <justin.r.harris@gmail.com>
2022-12-22 13:29:20 -0500

    updated serializers again

---
+++ kidcredible_project/kidcredible/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import User, Product, Review
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    reviews = serializers.HyperlinkedRelatedField(
+        view_name='review_detail',
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'password', 'reviews')
+
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    users = serializers.HyperlinkedRelatedField(
+        view_name='user_detail',
+        read_only=True
+    )
+    products = serializers.HyperlinkedRelatedField(
+        view_name='product_detail',
+        read_only=True
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'name', 'type', 'description',
+                  'image', 'users', 'rating')
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    reviews = ReviewSerializer(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'type', 'description',
+                  'image', 'rating', 'reviews')
